aa073126a513896db131be95897cbb4767fe2e0c
Neil Mastroianni <nf_mastroianni@hotmail.com>
2023-03-28 21:39:17 -0400

    added pages

---
+++ build.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const path = require('path')
+const minify = require('html-minifier').minify
+
+const allFiles = fs.readdirSync('./src/')
+const htmlFiles = allFiles.filter((file) => path.extname(file) === '.html')
+
+console.log('BUILDING...')
+htmlFiles.forEach((file) => {
+  fs.readFile(`./src/${file}`, 'utf-8', (err, data) => {
+    if (err) throw err
+    const result = data
+      .replace(/(sm:)/g, 'sm-')
+      .replace(/(md:)/g, 'md-')
+      .replace(/(lg:)/g, 'lg-')
+      .replace(/(xl:)/g, 'xl-')
+      .replace(/(hover:)/g, 'hover-')
+    const minresult = minify(result, { collapseWhitespace: true })
+    fs.writeFile(`./dist/${file}`, minresult, 'utf-8', (err) => {
+      if (err) throw err
+    })
+  })
+})
+fs.readFile('./dist/output.css', 'utf-8', (err, data) => {
+  if (err) throw err
+  const result = data
+    .replace(/(sm\\:)/g, 'sm-')
+    .replace(/(md\\:)/g, 'md-')
+    .replace(/(lg\\:)/g, 'lg-')
+    .replace(/(xl\\:)/g, 'xl-')
+    .replace(/(hover\\:)/g, 'hover-')
+  fs.writeFile('./dist/output.css', result, 'utf-8', (err) => {
+    if (err) throw err
+  })
+})
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*{.css,.html}'],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: 'rgb(0, 55, 103)',
+        sand: 'rgb(234, 230, 222)',
+        darksand: 'rgb(235, 217, 173)',
+      },
+    },
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('prettier-plugin-tailwindcss'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
