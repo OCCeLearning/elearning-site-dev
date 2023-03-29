b886a2d031321a31f75e20a3ffc71b990e53be30
Neil Mastroianni <nf_mastroianni@hotmail.com>
2023-03-29 06:46:00 -0400

    added restore feature

--- restore.js
+++ restore.js
@@ -1,16 +1,22 @@
 const fs = require('fs')
+const path = require('path')
 
+const allFiles = fs.readdirSync('./dist/')
+const htmlFiles = allFiles.filter((file) => path.extname(file) === '.html')
 console.log('RESTORING...')
 
-fs.readFile('./dist/index.html', 'utf-8', (err, data) => {
-  if (err) throw err
-  const result = data
-    .replace(/(sm-)/g, 'sm:')
-    .replace(/(md-)/g, 'md:')
-    .replace(/(lg-)/g, 'lg:')
-    .replace(/(xl-)/g, 'xl:')
-    .replace(/(hover-)/g, 'hover:')
-  fs.writeFile('./src/restored.html', result, 'utf-8', (err) => {
+htmlFiles.forEach((file) => {
+  fs.readFile(`./dist/${file}`, 'utf-8', (err, data) => {
     if (err) throw err
+    const result = data
+      .replace(/(sm-)/g, 'sm:')
+      .replace(/(md-)/g, 'md:')
+      .replace(/(lg-)/g, 'lg:')
+      .replace(/(xl-)/g, 'xl:')
+      .replace(/(hover-)/g, 'hover:')
+
+    fs.writeFile(`./src/restored-${file}`, result, 'utf-8', (err) => {
+      if (err) throw err
+    })
   })
 })
